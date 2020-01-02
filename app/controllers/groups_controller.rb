30a8c33690fbc6e7e95b4c87832d61e96d8b5126
sgnm-jp <ns.987mu.ssw@nifty.com>
2020-01-03 02:04:29 +0900

    Add: groups_controllerのcreateアクションの動作を追加

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -6,6 +6,12 @@ class GroupsController < ApplicationController
   end
 
   def create
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to root_path, notice: "グループを作成しました"
+    else
+      render :new
+    end
   end
 
   def edit
@@ -14,4 +20,8 @@ class GroupsController < ApplicationController
   def update
   end
 
+  private
+  def group_params
+    params.require(:group).permit(:name, user_ids: [] )
+  end
 end
